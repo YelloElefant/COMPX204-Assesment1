79d3a5cc0636076d2e2bac4916094f240fd2d012
YelloElefant <alextrotternz@gmail.com>
2024-07-16 23:29:56 +1200

    making reverse dns script
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

---
+++ reverse.java
@@ -0,0 +1,29 @@
+import java.net.*;
+import java.util.*;
+
+public class reverse {
+   public static void main(String[] args) {
+      if (args.length == 0) {
+         System.err.println("Usage: java reverse ipaddress");
+         return;
+      }
+      (Arrays.asList(args)).forEach((arg) -> {
+         resolveIpAdress(arg);
+      });
+
+   }
+
+   public static void resolveIpAdress(String ipAdressString) {
+      String[] ipAdressParts = ipAdressString.split("\\.");
+      byte[] ipAdress = new byte[4];
+      for (int i = 0; i < 4; i++) {
+         ipAdress[i] = (byte) Integer.parseInt(ipAdressParts[i]);
+      }
+      try {
+         InetAddress address = InetAddress.getByAddress(ipAdress);
+         System.out.println(ipAdressString + ": " + address.getHostName());
+      } catch (UnknownHostException e) {
+         System.err.println("Unable to resolve " + ipAdressString);
+      }
+   }
+}
