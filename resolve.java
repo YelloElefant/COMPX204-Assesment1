663eababa66a96e92dc7f4dc4e600e57db51b01f
YelloElefant <alextrotternz@gmail.com>
2024-07-16 17:40:03 +1200

    resolve script

---
+++ resolve.java
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class resolve {
+   public static void main(String[] args) {
+      if (args.length == 0) {
+         System.err.println("Usage: java resolve hostname");
+         return;
+      }
+      (Arrays.asList(args)).forEach((arg) -> {
+         resolveHostnames(arg);
+      });
+
+   }
+
+   public static void resolveHostnames(String hostName) {
+      try {
+         InetAddress address = InetAddress.getByName(hostName);
+         System.out.println(hostName + ": " + address.getHostAddress());
+      } catch (UnknownHostException e) {
+         System.err.println("Unable to resolve " + hostName);
+      }
+   }
+}
+
+// The resolve program takes a hostname as an argument and prints the
+// corresponding IP address. If the hostname cannot be resolved, the program
+// prints an error message.
+// The resolve program uses the InetAddress.getByName method to resolve the
+// hostname. This method returns an InetAddress object that represents the IP
+// address of the hostname. If the hostname cannot be resolved, the getByName
+// method throws an UnknownHostException .
+// The resolve program catches the UnknownHostException and prints an error
+// message.
+// Here is an example of running the resolve program:
+// $ java resolve www.google.com
