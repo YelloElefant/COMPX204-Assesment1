0aa0b90c4410a2d79565803bdb565c200fe657e7
YelloElefant <alextrotternz@gmail.com>
2024-07-17 19:01:57 +1200

    adding message sending to client
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- SocketClient.java
+++ SocketClient.java
@@ -4,14 +4,16 @@ import java.net.Socket;
 public class SocketClient {
    public static void main(String[] args) {
       if (args.length == 0) {
-         System.err.println("Usage: java SocketClient <ipaddress> <port>");
+         System.err.println("Usage: java SocketClient <ipaddress> <port> <message>");
          return;
       }
       String ip = args[0];
       int port = Integer.parseInt(args[1]);
+      String message = args[2];
       try {
          Socket socket = new Socket(ip, port);
          System.out.println("Connected to " + ip + " on port " + port);
+         socket.getOutputStream().write(message.getBytes());
          socket.close();
       } catch (IOException e) {
          System.err.println("Unable to connect to " + ip + " on port " + port);
